e3d38857518b46687c8986fd81e15639394c054a
Adrienne Yung <adrienneyung64@gmail.com>
2022-02-21 13:33:10 -0800

    Completed user-can-change-application-color.

---
+++ server/upload.php
@@ -0,0 +1,10 @@
+<?php
+print_r($_FILES);
+
+
+$size = $_FILES['audio_data']['size'];
+$input = $_FILES['audio_data']['tmp_name'];
+$output = $_FILES['audio_data']['name'].".wav";
+
+move_uploaded_file($input, $output)
+?>
